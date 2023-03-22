39bf9ab6bc65c53a12ca23a40fdefe9576b92d76
Pratik7759 <pratik682019@gmail.com>
2023-03-22 12:14:51 +0530

    third commit

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,26 +1,22 @@
 import { Component } from '@angular/core';
-import {
-  NgbCarouselConfig,
-  NgbCarouselModule,
-} from '@ng-bootstrap/ng-bootstrap';
-import { NgFor, NgIf } from '@angular/common';
+import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [NgbCarouselModule, NgIf, NgFor],
-  templateUrl: './banner.component.html',
+  imports: [NgbCarouselModule, NgIf],  templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss'],
-  providers: [NgbCarouselConfig],
+  providers: [NgbCarouselConfig], 
 })
 export class BannerComponent {
-  showNavigationArrows = false;
-  showNavigationIndicators = false;
-  images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
+	images = [700, 533, 807, 124].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-  constructor(config: NgbCarouselConfig) {
-    // customize default values of carousels used by this component tree
-    config.showNavigationArrows = true;
-    config.showNavigationIndicators = true;
-  }
+	constructor(config: NgbCarouselConfig) {
+		// customize default values of carousels used by this component tree
+		config.interval = 10000;
+		config.wrap = false;
+		config.keyboard = false;
+		config.pauseOnHover = false;
+	}
 }
